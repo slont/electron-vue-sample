533d31825803f8032c149fc5a20bb7ce249267fb
山崎 蓮馬 <renma.yamazaki@syncthought.com>
2018-04-09 18:39:14 +0900

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-vue-sample'
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  /* CSS */
+</style>
